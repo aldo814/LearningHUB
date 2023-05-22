2bf47eef1cfc576256c33410591e9d6404b6e8df
김은영 <66293497+aldo814@users.noreply.github.com>
2023-05-22 15:18:58 +0900

    .

--- html/js/run.js
+++ html/js/run.js
@@ -154,6 +154,15 @@ $(document).ready(function () {
             scrollTop: $(this.hash).offset().top
         }, 500)
     });
+    
+    $('.view_area_cont .tab_style04 a').on('click', function (e) {
+        e.preventDefault();
+        $(this).parent().addClass('active');
+         $(this).parent().siblings().removeClass('active')
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500)
+    });
 
 
     $('.popUp .view_area_cont .tab_style01 a').on('click', function (e) {
@@ -305,7 +314,7 @@ $(document).ready(function () {
     $(".datep").datepicker({
 
         showOn: "both", // 버튼과 텍스트 필드 모두 캘린더를 보여준다.
-
+        buttonImage: "./images/calendar_ico.png",
 
         changeMonth: true, // 월을 바꿀수 있는 셀렉트 박스를 표시한다.
 
@@ -339,6 +348,45 @@ $(document).ready(function () {
 
 
     });
+    
+    $(".datep02").datepicker({
+
+        showOn: "both", // 버튼과 텍스트 필드 모두 캘린더를 보여준다.
+        buttonImage: "./images/date.png",
+
+        changeMonth: true, // 월을 바꿀수 있는 셀렉트 박스를 표시한다.
+
+        changeYear: true, // 년을 바꿀 수 있는 셀렉트 박스를 표시한다.
+
+        minDate: '-100y', // 현재날짜로부터 100년이전까지 년을 표시한다.
+
+        nextText: '다음 달', // next 아이콘의 툴팁.
+
+        prevText: '이전 달', // prev 아이콘의 툴팁.
+
+        numberOfMonths: [1, 1], // 한번에 얼마나 많은 월을 표시할것인가. [2,3] 일 경우, 2(행) x 3(열) = 6개의 월을 표시한다.
+
+        stepMonths: 3, // next, prev 버튼을 클릭했을때 얼마나 많은 월을 이동하여 표시하는가. 
+
+        yearRange: 'c-100:c+10', // 년도 선택 셀렉트박스를 현재 년도에서 이전, 이후로 얼마의 범위를 표시할것인가.
+
+        showButtonPanel: true, // 캘린더 하단에 버튼 패널을 표시한다. 
+
+        currentText: '오늘 날짜', // 오늘 날짜로 이동하는 버튼 패널
+
+        closeText: '닫기', // 닫기 버튼 패널
+
+        dateFormat: "yy.mm.dd", // 텍스트 필드에 입력되는 날짜 형식.
+
+        showMonthAfterYear: true, // 월, 년순의 셀렉트 박스를 년,월 순으로 바꿔준다. 
+
+        dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'], // 요일의 한글 형식.
+
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'], // 월의 한글 형식.
+
+
+    });
+
 
     // 이미지 file
     var container = $('.file_img'),
@@ -394,6 +442,16 @@ $(document).ready(function () {
         $(this).next('.fold').toggleClass('open')
     });
 
+    var ctr = $('#side').height();
+    $('.m_flex').css('top', ctr - 20);
+
+    $('.con_list02 dl').each(function () {
+        if (($(this).find('dd').height()) < 25) {
+            $(this).find('dd').addClass('mt20')
+        } else {
+
+        }
+    })
 
 
 
